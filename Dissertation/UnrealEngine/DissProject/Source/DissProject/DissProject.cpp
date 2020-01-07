ac8ee0477154ef48570a2d56113d372061cda546
Mason Powell <mj.powell@hotmail.com>
2020-01-07 13:05:16 +0000

    Base UE4 project files
    Additional global .gitignore as GitKraken doesn't recognise nested .gitignore files

---
+++ Dissertation/UnrealEngine/DissProject/Source/DissProject/DissProject.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "DissProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DissProject, "DissProject" );
