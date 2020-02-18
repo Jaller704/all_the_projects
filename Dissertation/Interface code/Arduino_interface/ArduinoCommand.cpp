171c8beb217af7617e6d37f3af123107c85e9ceb
Mason <mj.powell@hotmail.com>
2020-02-18 21:23:01 +0000

    Completed initial version of ArduinoCommand class

--- Dissertation/Interface code/Arduino_interface/ArduinoCommand.cpp
+++ Dissertation/Interface code/Arduino_interface/ArduinoCommand.cpp
@@ -7,7 +7,7 @@ ArduinoCommand::ArduinoCommand(string raw_command) {
 	size_t first_space = raw_command.find_first_of("\t\n\v\f\r ", first_letter);
 	
 	//Pull this name out of the string and set the command_name
-	setCommandName(raw_command.substr(first_letter, (first_letter - (first_space - 1))));
+	this->command_name = raw_command.substr(first_letter, (first_letter - (first_space - 1)));
 
 	//Find where the first parameter starts
 	size_t first_param = raw_command.find_first_not_of("\t\n\v\f\r ", first_space);
@@ -15,31 +15,58 @@ ArduinoCommand::ArduinoCommand(string raw_command) {
 	string temp_string = raw_command.substr(first_param);
 
 	vector<string> params(1);
+	//Split the parameters up into individual strings
 	stringSplit(temp_string, params, ' ');
-	int temp_params[params.size];
 
+	//Convert the strings into integers and copy into the command_params vector
 	for (string i : params) {
-		temp_params[i] = stoi(i);
+		this->command_params.emplace_back(stoi(i));
 	}
 
+	//Recombine the name and parameters to create a string fomatted for the serial console
+	stringstream ss;
+	ss << command_name;
+	for (int i : command_params) {
+		ss << " " << i;
+	}
+	ss << "\r\n";
+
+	this->full_command = unique_ptr<string>(new string(ss.str()));
+
 }
 
-ArduinoCommand::ArduinoCommand(string command_name, int command_params[]) {
+ArduinoCommand::ArduinoCommand(string name, vector<int> params) {
+	//Find the index of the first letter in the string, this should be the first letter of the command name
+	size_t first_letter = name.find_first_not_of("\t\n\v\f\r ");
+	//Find where the command name ends by looking for the first bit of whitespace after the first letter
+	size_t first_space = name.find_first_of("\t\n\v\f\r ", first_letter);
 
+	//Pull this name out of the string and set the command_name
+	this->command_name = name.substr(first_letter, (first_letter - (first_space - 1)));
+
+	//Do a deep copy of the input parameters for robustness
+	for (int i : params) {
+		this->command_params.emplace_back(i);
+	}
+
+	//Combine the name and parameters to create a string fomatted for the serial console
+	stringstream ss;
+	ss << command_name;
+	for (int i : command_params) {
+		ss << " " << i;
+	}
+	ss << "\r\n";
+
+	this->full_command = unique_ptr<string>(new string(ss.str()));
 }
 
-string ArduinoCommand::getSerialCommand() {
-	return string();
+string ArduinoCommand::getSerialCommand()
+{
+	return *full_command;
 }
 
-void ArduinoCommand::setCommandName(string command_name) {
-}
-
-void ArduinoCommand::setCommandParams(int command_params[]) {
-}
-
-void ArduinoCommand::stringSplit(const string& str, vector<string>& split_string, char delim = ' ') {
-	stringstream ss(str);
+void ArduinoCommand::stringSplit(const string& input, vector<string>& split_string, char delim = ' ') {
+	stringstream ss(input);
 	string token;
 	while (getline(ss, token, delim)) {
 		split_string.emplace_back(token);
--- Dissertation/Interface code/Arduino_interface/ArduinoCommand.h
+++ Dissertation/Interface code/Arduino_interface/ArduinoCommand.h
@@ -8,16 +8,33 @@ using namespace std;
 class ArduinoCommand
 {
 public:
+	/*
+	* Takes in raw string representation of the entire command
+	* and splits it into the name and parameters, along with trimming 
+	* out any whitespace before or after the name.
+	*/
 	ArduinoCommand(string raw_command);
-	ArduinoCommand(string command_name, int command_params[]);
 
+	/*
+	* Takes in the command name and the parameters, trims the whitespace 
+	* out of the name string.
+	*/
+	ArduinoCommand(string name, vector<int> params);
+
+	/*
+	* Return the full_command which has been formatted for the serial console.
+	*/
 	string getSerialCommand();
 private:
-	void setCommandName(string command_name);
-	void setCommandParams(int command_params[]);
-	void stringSplit(const string& str, vector<string>& split_string, char delim = ' ');
+	/*
+	* Splits theinput string based on a delimiter and puts the contents 
+	* into a string vector.
+	*/
+	void stringSplit(const string& input, vector<string>& split_string, char delim = ' ');
+
 	string command_name;
-	int command_params[];
+	vector<int> command_params;
+	unique_ptr<string> full_command;
 
 
 };
