e56abadd47963c8ce39c47a05cb087536c5d0f12
Mason Powell <mj.powell@hotmail.com>
2020-03-02 17:44:40 +0000

    Created initial MotorInterface class for the arduino motor control

---
+++ Dissertation/Arduino/MotorInterface/MotorInterface.ino
@@ -0,0 +1,24 @@
+#include <Stepper.h>
+#include <Servo.h>
+#include <SoftwareSerial.h>
+#include <SerialCommand.h>
+
+Servo fore_servo
+Servo twrist_servo
+Servo bwrist_servo
+Servo lwrist_servo
+Servo rwrist_servo
+Stepper elbow_step
+Stepper shoulder_step
+Stepper hume_step
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
