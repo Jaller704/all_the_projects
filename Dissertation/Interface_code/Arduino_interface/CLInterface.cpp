b201fd1bf59eb24a4a7bad119abaefc6d5ccab4e
MasonVRLaptop <mj.powell@hotmail.com>
2020-04-26 21:47:01 +0100

    Updated gitignore for debug files and changed folder names

---
+++ Dissertation/Interface_code/Arduino_interface/CLInterface.cpp
@@ -0,0 +1,62 @@
+#include "CLInterface.h"
+
+int main() {
+	ArduinoInterface* a_interface = new ArduinoInterface(CLInterface::selectSerialPort());
+	cout << "Beginning the interface loop" << endl;
+	CLInterface::interfaceLoop(a_interface);
+	return 0;
+}
+
+string inputTrim(string raw_input) {
+	//Find the index of the first letter in the string
+	size_t first_letter = raw_input.find_first_not_of("\t\n\v\f\r ");
+	//Find where the command name ends by looking for the first bit of whitespace after the first letter
+	size_t line_break = raw_input.find_first_of("\t\n\v\f\r ", first_letter);
+
+	raw_input = raw_input.substr(first_letter, (first_letter - (line_break - 1)));
+
+	return raw_input;
+}
+
+Serial CLInterface::selectSerialPort() {
+	string input_port_str;
+	int input_port;
+	stringstream  port_name_str;
+	cout << "Enter the COM port number that the Arduino is connected to (usually 14):" << endl;
+	getline(cin, input_port_str);
+
+	//Takes just the integer from the input 
+	stringstream(input_port_str) >> input_port;
+
+
+	port_name_str << "\\\\.\\COM" << input_port;
+	
+	const std::string& tmp = port_name_str.str();
+	//const char* port_name = tmp.c_str();
+	return Serial(tmp.c_str());
+}
+
+void CLInterface::interfaceLoop(ArduinoInterface* a_interface) {
+	string raw_input;
+	cout << "interface loop ready" << endl;
+
+	cout << "Input END to stop the loop and close the program" << endl;
+
+	cout << "Enter command: ";
+
+	getline(cin, raw_input);
+
+	string input = inputTrim(raw_input);
+
+	while (input != "END") {
+		
+		ArduinoCommand command = ArduinoCommand(input);
+
+		cout << a_interface->writeRead(command) << endl;
+
+		getline(cin, raw_input);
+
+		input = inputTrim(raw_input);
+	}
+}
+
